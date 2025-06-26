e53aa87f9afd0a4d14e51bd5513be2b0c8fa4ed1
nxtuan94 <123918387+nxtuan94@users.noreply.github.com>
2025-06-27 01:41:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import discord
 from discord.ext import commands
 from config import TOKEN
 from loader import setup_all
-from keepalive import keep_alive
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -14,7 +13,5 @@ async def on_ready():
     print(f"Bot is ready as {bot.user}")
     await setup_all(bot)
 
-
-keep_alive()  # Gọi trước bot.run()
 
 bot.run(TOKEN)
